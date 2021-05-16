7ea2d4ed990697bc90567a6e70984d610a2f8f45
David Yee <github@mail.davidjyee.dev>
2021-05-16 16:22:18 -0400

    Added a listener that shows and closes the atm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [showUI, setShowUI] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('message', (event) => {
+      if (event.data.type === 'atm-visibility') {
+        setShowUI(event.data.visibility);
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> This is a test </h1>
-      </header>
+      <header className="App-header">{showUI && <h1> This is a test </h1>}</header>
     </div>
   );
 }
