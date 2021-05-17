b4e1ed56964f66e902b2caf9cbea95f066257e54
David Yee <github@mail.davidjyee.dev>
2021-05-17 16:04:31 -0400

    Added some nui callback messages

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,62 @@ function App() {
     window.addEventListener('message', (event) => {
       if (event.data.type === 'atm-visibility') {
         setShowUI(event.data.visibility);
+
+        fetch(`https://atm-esx-react-example/focus`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: JSON.stringify({
+            focus: event.data.visbility,
+          }),
+        });
       }
     });
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">{showUI && <h1> This is a test </h1>}</header>
+      <header className="App-header">
+        {showUI && (
+          <div>
+            <button
+              onClick={() => {
+                fetch(`https://atm-esx-react-example/deposit`, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                  },
+                  body: JSON.stringify({
+                    amount: 50,
+                  }),
+                })
+                  .then((res) => res.json())
+                  .then((res) => console.log(JSON.stringify(res)));
+              }}
+            >
+              Deposit
+            </button>
+            <button
+              onClick={() => {
+                setShowUI(false);
+
+                fetch(`https://atm-esx-react-example/focus`, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                  },
+                  body: JSON.stringify({
+                    focus: false,
+                  }),
+                });
+              }}
+            >
+              Exit
+            </button>
+          </div>
+        )}
+      </header>
     </div>
   );
 }
