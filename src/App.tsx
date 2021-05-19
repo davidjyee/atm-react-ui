9d6f8d5c48ffd06d0079ed5a8310973326b1eebf
David Yee <github@mail.davidjyee.dev>
2021-05-19 19:09:04 -0400

    Changed provider to be in index instead of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.scss';
 
-import store, { ThunkDispatch, IStoreState } from './store';
-import { Provider, useDispatch, useSelector } from 'react-redux';
+import { ThunkDispatch, IStoreState } from './store';
+import { useDispatch, useSelector } from 'react-redux';
 import { showUI, deposit } from './actions';
 
 function App() {
@@ -10,28 +10,26 @@ function App() {
   const show = useSelector((state: IStoreState) => state.messages.show);
 
   return (
-    <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          {show && (
-            <div>
-              <button
-                className="mdc-button mdc-button--raised"
-                onClick={() => dispatch(deposit(50)).then(console.log)}
-              >
-                <span className="mdc-button__label"> Deposit </span>
-              </button>
-              <button
-                className="mdc-button mdc-button--raised"
-                onClick={() => dispatch(showUI(false))}
-              >
-                <span className="mdc-button__label"> Exit </span>
-              </button>
-            </div>
-          )}
-        </header>
-      </div>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        {show && (
+          <div>
+            <button
+              className="mdc-button mdc-button--raised"
+              onClick={() => dispatch(deposit(50)).then(console.log)}
+            >
+              <span className="mdc-button__label"> Deposit </span>
+            </button>
+            <button
+              className="mdc-button mdc-button--raised"
+              onClick={() => dispatch(showUI(false))}
+            >
+              <span className="mdc-button__label"> Exit </span>
+            </button>
+          </div>
+        )}
+      </header>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
+import store from './store';
+import { Provider } from 'react-redux';
+
 //Register the event listeners by running the file
 import './registerEventListeners';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
