068def38ec590daf630ffa733fcbdcbf9bee1c60
David Yee <github@mail.davidjyee.dev>
2021-05-19 19:03:43 -0400

    Fixed messages

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import messageReducer from './messageReducer';
 
 export default combineReducers({
-  message: messageReducer,
+  messages: messageReducer,
 });
