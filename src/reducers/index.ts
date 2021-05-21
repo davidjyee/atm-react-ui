8ccc2c0a818f20d2e3699693cc8b596b6ed1a9b5
David Yee <github@mail.davidjyee.dev>
2021-05-21 13:52:16 -0400

    Added a status reducer for current status

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import messageReducer from './messageReducer';
 import accountReducer from './accountReducer';
+import statusReducer from './statusReducer';
 
 export default combineReducers({
   messages: messageReducer,
   account: accountReducer,
+  status: statusReducer,
 });
---
+++ src/reducers/statusReducer.ts
@@ -0,0 +1,49 @@
+import { AnyAction } from 'redux';
+import { START_DEPOSIT, FINISH_DEPOSIT } from '../actions';
+
+interface status {
+  name: string;
+  id: number;
+  cash: number;
+  transactionLock: boolean;
+}
+
+const initialState: status = {
+  name: 'Akihiro Sakamoto',
+  id: 816,
+  cash: 100,
+  transactionLock: false,
+};
+
+function transact(account: status, action: AnyAction): status {
+  const newState = {
+    ...account,
+    transactionLock: false,
+  };
+
+  if (action.success) {
+    switch (action.type) {
+      case FINISH_DEPOSIT:
+        newState.cash -= action.amount;
+        break;
+      default:
+        break;
+    }
+  }
+
+  return newState;
+}
+
+export default function statusReducer(state = initialState, action: AnyAction): status {
+  switch (action.type) {
+    case START_DEPOSIT:
+      return {
+        ...state,
+        transactionLock: true,
+      };
+    case FINISH_DEPOSIT:
+      return transact(state, action);
+    default:
+      return state;
+  }
+}
